5eb0e528cf75edd3b5a462d6a6f85809ac7b0841
Cristian Villegas <cnvillegaschavez@gmail.com>
2018-12-22 23:11:35 -0500

    Fix

--- WcfServicePoderosaDemo/IEmployeeService.cs
+++ WcfServicePoderosaDemo/IEmployeeService.cs
@@ -5,7 +5,7 @@ using WcfServicePoderosa.Contracts.Contracts;
 namespace WcfServicePoderosaDemo
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
-    [ServiceContract)]
+    [ServiceContract]
     public interface IEmployeeService
     {
         [OperationContract]
